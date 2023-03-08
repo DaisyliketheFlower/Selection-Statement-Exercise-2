6068717688f0c6008e1c8905ba4f08659f5977f3
daisy.omoikirodah <thesmexxiichic@gmail.com>
2023-03-08 17:18:14 -0500

    done

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Whats your favourite subject?");
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+
+                case "english":
+                  Console.WriteLine("English is Amazing!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is rocks!");
+                    break;
+                case "physics":
+                    Console.WriteLine("Physics is tough but awesome!");
+                    break;
+                case "physical education":
+                    Console.WriteLine("Physical Education is my favorite!");
+                    break;
+
+                case "math":
+                    Console.WriteLine("Math sucks but i'll manage it!");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Dont like that subject!");
+                    break;
+        }
         }
     }
 }
